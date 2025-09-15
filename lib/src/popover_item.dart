614c776b534887f71d4dd8380ae6a70ca8a9884f
Manuel Di Criscito <manu12.m189@gmail.com>
2025-09-15 18:01:47 +0200

    undo scrollable popover test

--- lib/src/popover_item.dart
+++ lib/src/popover_item.dart
@@ -83,7 +83,7 @@ class _PopoverItemState extends State<PopoverItem> {
             child: ClipRSuperellipse(
               borderRadius: BorderRadius.circular(widget.radius ?? 5),
               child: Material(
-                child: SingleChildScrollView(child: widget.child),
+                child: widget.child,
                 color: widget.backgroundColor,
               ),
             ),
